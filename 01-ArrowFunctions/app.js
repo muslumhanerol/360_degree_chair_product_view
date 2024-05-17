1bc34fb049a7ac269cd7760e153836c722533880
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-18 01:08:40 +0300

    Update app.js

--- 01-ArrowFunctions/app.js
+++ 01-ArrowFunctions/app.js
@@ -1,5 +1,23 @@
-function selamVer(hey){
+function selamVer(){
     console.log("Merhaba, burası klasik bir function");
 };
 
-selamVer(hey);
+const selamVer2 = () => {
+    console.log ("Merhaba, burası bir arrow function");
+};
+
+const selamVer3 = (ad) => { //Parametreli arrow function.
+    console.log("Merhaba " + ad);
+};
+
+
+const topla = (sayi1,sayi2) => { //2 Parametreli arrow function.
+    return sayi1 + sayi2
+};
+
+
+
+// selamVer();
+// selamVer2();
+// selamVer3("Müslüm");
+console.log(topla(4,6));
