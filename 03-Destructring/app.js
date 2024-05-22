55e4d0626e43a22c0f36b68a027caabb1ae292af
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-23 00:29:11 +0300

    Update app.js

--- 03-Destructring/app.js
+++ 03-Destructring/app.js
@@ -1,3 +1,8 @@
 let fullName = ["Kemal","Candan"];
-let firstName = fullName[0];
-let lastName = fullName[1];
+// let firstName = fullName[0]; //firstName dediğimizde kemal i çağırmış olduk.
+// let lastName = fullName[1];
+
+
+let [firstName, lastName] = fullName;
+console.log(firstName,lastName);
+
